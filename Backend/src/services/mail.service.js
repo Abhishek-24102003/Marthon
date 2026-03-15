0b5daa09147660f25c66426cab7b3c3621c7f3f0
Abhishek raj lakhera <abhishekrajlakhera101@gmail.com>
2026-03-15 23:31:51 +0530

    gmail service

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -3,9 +3,9 @@ import nodeMailer from "nodemailer";
 const transporter = nodeMailer.createTransport({
     service: "gmail",
     auth: {
-        user: "abhishekrajlakhera101@gmail.com",
-        // pass:"sgzpcumksnpxoffr"
-        pass:"zfcbtsbwmbhvonhe"
+        user: "abhishekrajlakhera3579@gmail.com",
+        pass:"sgzpcumksnpxoffr"
+        // pass:"zfcbtsbwmbhvonhe"
     }
 })
 export const sendmail = async (email,subject,body) => {
