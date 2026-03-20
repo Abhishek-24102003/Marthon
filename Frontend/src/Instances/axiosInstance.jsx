b1aef960f1655f9c7730b06c41b1e2f80ceb1887
Abhishek raj lakhera <abhishekrajlakhera101@gmail.com>
2026-03-20 14:45:36 +0530

    baseUrl update

--- Frontend/src/Instances/axiosInstance.jsx
+++ Frontend/src/Instances/axiosInstance.jsx
@@ -1,6 +1,6 @@
 import axios from "axios"
  export const axiosInstance =  axios.create({
-     baseURL: "https://marthon.vercel.app/",
+     baseURL: "https://marthon.vercel.app/api",
      withCredentials:true
 })
 
