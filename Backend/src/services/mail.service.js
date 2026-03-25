c4f5fe9aab64bc39a8aa8794eca4b462610fdbbb
Abhishek raj lakhera <abhishekrajlakhera101@gmail.com>
2026-03-25 09:40:55 +0530

    Mail service debug

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,11 +1,15 @@
 import nodeMailer from "nodemailer";
 
 const transporter = nodeMailer.createTransport({
-    service: "gmail",
+   host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for port 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS
-    }
+        pass: process.env.EMAIL_PASS
+    },
+    // Add a connection timeout to prevent the crash
+    connectionTimeout: 10000, // 10 seconds
 })
 export const sendmail = async (email,subject,body) => {
     const option = {
