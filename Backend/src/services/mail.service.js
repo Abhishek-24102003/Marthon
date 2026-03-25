23ba917aaccb0615c0e081aebafaf5f98b85a452
Abhishek raj lakhera <abhishekrajlakhera101@gmail.com>
2026-03-25 09:45:28 +0530

    mail dns

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,15 +1,18 @@
 import nodeMailer from "nodemailer";
 
 const transporter = nodeMailer.createTransport({
-   host: "smtp.gmail.com",
+ host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for port 465, false for other ports
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    // Add a connection timeout to prevent the crash
-    connectionTimeout: 10000, // 10 seconds
+    // This forces Node to use IPv4 instead of IPv6
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
+    dnsProjection: "ipv4first"
 })
 export const sendmail = async (email,subject,body) => {
     const option = {
